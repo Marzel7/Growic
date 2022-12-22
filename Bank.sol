844572653a341a869f400800b08edc174a5d9c62
kkeaveney <kevinkeaveney@gmail.com>
2022-12-22 16:56:56 +0000

    setUserDetails with calldata args

--- Bank.sol
+++ Bank.sol
@@ -4,11 +4,27 @@ pragma solidity ^0.8.0;
 contract Bank {
     mapping(address => uint256) public balances;
 
+    struct User {
+        string name;
+        uint256 age;
+    }
+
+    User public user;
+
     function deposit(uint256 _amount) external payable {
         balances[msg.sender] = _amount;
     }
 
     function checkBalance() external view returns (uint256) {
         return balances[msg.sender];
+    }
+
+    function setUserDetails(string calldata _name, uint256 _age) public {
+        user.name = _name;
+        user.age = _age;
+    }
+
+    function getUserDetail() public view returns (User memory) {
+        return user;
     }
 }
